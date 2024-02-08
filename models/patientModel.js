c0b0fce41e8f37bdbe51f07dc66318a26d9bdf0d
Ayshahariss <ayshaharis432@gmail.com>
2024-02-08 15:58:15 +0530

    new branch

---
+++ models/patientModel.js
@@ -0,0 +1,13 @@
+const mongoose=require("mongoose")
+
+const patientSchema = new  mongoose.Schema(
+    {
+            name:String,
+            phn:String,
+            address:String,
+            Symptom:String
+    }
+
+)
+
+module.exports=mongoose.model("patient",patientSchema)
